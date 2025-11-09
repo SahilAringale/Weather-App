1b92ab06264489e9d730085f0b9cd4c93ad56b69
Sahil Aringale <sahilaringale@gmail.com>
2025-11-09 11:01:45 +0530

    Initial commit - Weather App

---
+++ script.js
@@ -0,0 +1,30 @@
+const apiKey = "f0420699f644ec277322b37d413d1a48"; 
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const cityInput = document.getElementById("cityInput");
+const weatherResult = document.getElementById("weatherResult");
+
+getWeatherBtn.addEventListener("click", () => {
+    const cityName = cityInput.value;
+    if(cityName === "") {
+        weatherResult.innerHTML = "Please enter a city name";
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.cod === "404") {
+            weatherResult.innerHTML = "City not found!";
+            return;
+        }
+        weatherResult.innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+        `;
+    })
+    .catch(error => {
+        weatherResult.innerHTML = "Error fetching data";
+        console.error(error);
+    });
+});
